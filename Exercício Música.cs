c4e3b71a986c88482532796fd5e7d5b8618f8dfa
MauricioM06 <78988280+MauricioM06@users.noreply.github.com>
2021-03-10 12:53:10 -0300

    Add files via upload

---
+++ Exercício Música.cs
@@ -0,0 +1,112 @@
+using System;
+
+namespace Música
+{
+
+    public class Program 
+    {
+        public static void Main( string [] args )
+        {
+           Console.Clear();
+           Console.WriteLine();
+           Console.ForegroundColor = ConsoleColor.Yellow;
+           Console.WriteLine("Tossie Slide - Drake");
+           Console.WriteLine();
+		   Console.ForegroundColor = ConsoleColor.Cyan;
+           Console.WriteLine("Black leather gloves, no sequins");
+           Console.WriteLine("Buckles on the jacket, it's Alyx shit");
+           Console.WriteLine("Nike crossbody, got a piece in it");
+           Console.WriteLine("Got a dance, but it's really on some street shit");
+           Console.WriteLine("I'ma show you how to get it");
+           Console.WriteLine();
+           Console.WriteLine("It go right foot up, left foot, slide");
+           Console.WriteLine("Left foot up, right foot, slide");
+           Console.WriteLine("Basically I'm sayin' either way, we 'bout to slide,ayy");
+           Console.WriteLine("Can't let this one slide, ayy");
+           Console.WriteLine();
+           Console.ForegroundColor = ConsoleColor.Red;
+           Console.WriteLine("Don't you wanna dance with me? No?");
+           Console.WriteLine("I could dance like Michael Jackson");
+           Console.WriteLine("I could give you a thug passion");
+           Console.WriteLine("It's a Thriller in the trap where we from");
+           Console.WriteLine("Baby, don't you wanna dance with me? No?");
+           Console.WriteLine("I could dance like Michael Jackson");
+           Console.WriteLine("I could give you satisfaction");
+           Console.WriteLine("And you know we out here every day with it");
+           Console.WriteLine("I'ma show you how to get it");
+           Console.WriteLine();
+           Console.WriteLine("It go right foot up, left foot, slide");
+           Console.WriteLine("Left foot up, right foot, slide");
+           Console.WriteLine("Basically I'm sayin' either way, we 'bout to slide,ayy");
+           Console.WriteLine("Can't let this one slide, ayy");
+           Console.WriteLine();
+           Console.ResetColor();
+           Console.WriteLine("Two thousand shorties wanna tie the knot, ayy, yeah");
+           Console.WriteLine("Two hundred shooters on my brother's block, woah, yeah");
+           Console.WriteLine("Petal off rose like I love her not, maybe not");
+           Console.WriteLine("I don't know what's wrong with me, I can't stop, woah, yeah");
+           Console.WriteLine("Won't stop, woah, yeah, never stop");
+           Console.WriteLine("Got so many opps, I be mistakin' opps for other opps");
+           Console.WriteLine("Got so many people that I love out of troubled spots");
+           Console.WriteLine("Other than the family I got, it's either you or me");
+           Console.WriteLine("That's just how I think, it's either you or me");
+           Console.WriteLine("This life got too deep to you, baby");
+           Console.WriteLine("Two or three of us about do creep where they stayin'");
+           Console.WriteLine();
+           Console.ForegroundColor = ConsoleColor.Cyan;
+           Console.WriteLine("Black leather gloves, no sequins");
+           Console.WriteLine("Buckles on the jacket, it's Alyx shit");
+           Console.WriteLine("Nike crossbody, got a piece in it");
+           Console.WriteLine("Got a dance, but it's really on some street shit");
+           Console.WriteLine("I'ma show you how to get it");
+           Console.WriteLine();
+           Console.WriteLine("It go right foot up, left foot, slide");
+           Console.WriteLine("Left foot up, right foot, slide");
+           Console.WriteLine("Basically I'm sayin' either way, we 'bout to slide,ayy");
+           Console.WriteLine("Can't let this one slide, ayy");
+           Console.WriteLine();
+           Console.ResetColor();
+           Console.WriteLine("Tossie Slide, then I hit it double-time");
+           Console.WriteLine("Then I hit spin 'cause we spun their block a couple times");
+           Console.WriteLine("If it's not the right time, there'll always be another time");
+           Console.WriteLine("I'm not even trippin', we'll just see 'em in the summertime, woah, yeah");
+           Console.WriteLine("Can't describe the pressure I be puttin' on myself, yeah");
+           Console.WriteLine("Really, I just can't afford to lose nobody else, yeah");
+           Console.WriteLine("If they movin' shaky, we'll just do this shit ourselves, woah");
+           Console.WriteLine("If I'm movin' shaky, Chubbs'll do this shit himself, yeah");
+           Console.WriteLine("Solo niggas, only YOLO, for real");
+           Console.WriteLine("Heard a lot about you but we don't know for real");
+           Console.WriteLine("Next time, guarantee the truth'll get revealed");
+           Console.WriteLine();
+           Console.ForegroundColor = ConsoleColor.Cyan;
+           Console.WriteLine("Black leather gloves, no sequins");
+           Console.WriteLine("Buckles on the jacket, it's Alyx shit");
+           Console.WriteLine("Nike crossbody, got a piece in it");
+           Console.WriteLine("Got a dance, but it's really on some street shit");
+           Console.WriteLine("I'ma show you how");
+           Console.WriteLine();
+           Console.WriteLine("It go right foot up, left foot, slide");
+           Console.WriteLine("Left foot up, right foot, slide");
+           Console.WriteLine("Basically I'm sayin' either way, we 'bout to slide,ayy");
+           Console.WriteLine("Can't let this one slide, ayy");
+           Console.WriteLine();
+           Console.ForegroundColor = ConsoleColor.Red;
+           Console.WriteLine("Don't you wanna dance with me? No?");
+           Console.WriteLine("I could dance like Michael Jackson");
+           Console.WriteLine("I could give you a thug passion");
+           Console.WriteLine("It's a Thriller in the trap where we from");
+           Console.WriteLine("Baby, don't you wanna dance with me? No?");
+           Console.WriteLine("I could dance like Michael Jackson");
+           Console.WriteLine("I could give you satisfaction");
+           Console.WriteLine("And you know we out here every day with it");
+           Console.WriteLine("I'ma show you how to get it");
+           Console.WriteLine();
+           Console.WriteLine("It go right foot up, left foot, slide");
+           Console.WriteLine("Left foot up, right foot, slide");
+           Console.WriteLine("Basically I'm sayin' either way, we 'bout to slide,ayy");
+           Console.WriteLine("Can't let this one slide");
+           Console.WriteLine();
+           Console.ResetColor();
+        }
+    }
+}
